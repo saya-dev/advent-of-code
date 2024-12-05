eee87ffd442afab9bc4f1fed38e2b9ecaeec0e6f
saya <sametcetinkaya00s@gmail.com>
2024-12-05 13:55:09 +0300

    day-02

---
+++ 2024/02/main.go
@@ -0,0 +1,74 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func diff(a, b int) int {
+   if a < b {
+      return b - a
+   }
+   return a - b
+}
+
+func main()  {
+	file, err := os.Open("input.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+
+	safeReports := 0
+	fixableReports := 0
+
+	for scanner.Scan() {
+		line := scanner.Text()
+		levels := []int{}
+
+		for {
+			level, rest, found := strings.Cut(line, " ")
+
+			levelInt, err := strconv.Atoi(level)
+			if err != nil {
+				log.Fatal(err)
+			}
+			levels = append(levels, levelInt)
+
+			if !found {
+				break
+			}
+			line = rest
+		}
+		
+		increasing := levels[0] < levels[1]
+		errors := 0
+
+		for i := 0; i < len(levels) - 1; i++ {
+			difference := diff(levels[i], levels[i+1])
+
+			if increasing != (levels[i] < levels[i+1]) || difference < 1 || difference > 3  {
+				if errors == 2 {
+					break	
+				}
+				errors += 1
+			}
+		}
+
+		if errors == 0 {
+			safeReports += 1
+		} else if errors == 1 {
+			fixableReports += 1
+		}
+	}
+
+	fmt.Println("Safe Reports:", safeReports)
+	fmt.Println("Fixable Reports:", fixableReports)
+	fmt.Println("Safe & Fixable Reports:", safeReports + fixableReports)
+}
